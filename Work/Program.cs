8cbd99bc940850ea978b4e4a38dc7a9b3452cf4e
izl0m-99 <danill.kuraev@yandex.ru>
2019-09-18 22:17:12 +0300

    firs commit

---
+++ Work/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Mark!");
+            Console.WriteLine("This is Daniil!");
+            Console.ReadKey();
+        }
+    }
+}
