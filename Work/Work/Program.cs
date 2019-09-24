deb751a0adbd9895c8c06cb9703d5f9a08315cd9
izl0m-99 <55406197+izl0m-99@users.noreply.github.com>
2019-09-24 22:17:15 +0300

    New commit
    Good commit

---
+++ Work/Work/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Mark!");
+            Console.WriteLine("This is Daniil!");
+            Console.WriteLine("Start project!");
+            Console.WriteLine("Foo!");
+            Console.ReadKey();
+        }
+    }
+}
